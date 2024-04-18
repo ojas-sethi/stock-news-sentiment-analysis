8dd114bc8f51a10e10262f74adb77fcb6e62f9b9
Arnav Sharma <sharma55@wisc.edu>
2024-04-17 23:45:33 -0500

    Add boilerplate code to read news data

---
+++ src/data_acquisition_pipeline/pipeline.py
@@ -0,0 +1,15 @@
+import sys, json
+
+
+
+# API KEYS (If necessary)
+
+
+# Collection of tickers for which we want to fetch news articles
+
+def main():
+
+    return
+
+if __name__ == '__main__':
+    main()
