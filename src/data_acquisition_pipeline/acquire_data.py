84eb52b253b5ad4cb9fb5bdcff932c5fe10ba8ab
Arnav Sharma <sharma55@wisc.edu>
2024-04-20 08:51:22 -0500

    Add debug option
     - Disable api calls for local development

--- src/data_acquisition_pipeline/acquire_data.py
+++ src/data_acquisition_pipeline/acquire_data.py
@@ -23,6 +23,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--ticker_file", type=str, default='ticker_data.json', help="Path to the JSON file that contains ticker names and dates.")
     parser.add_argument("--output_dir", type=str, default='../../data', help="Path to the directory where acquired data should be stored.")
+    parser.add_argument("--debug", default=False, help="Setting flag to true disables api requests being sent out.", action="store_true")
     #Add any more arguments as and when needed
     args = parser.parse_args()
 
@@ -47,15 +48,14 @@ def main():
 
             to_date = date.fromisoformat(date_str)
             from_date = to_date - timedelta(days=NUM_DAYS)
-            print(f"From Date: {from_date.isoformat()}")
-            print(f"To Date: {to_date.isoformat()}")
-            data = fetch_news_data_for_ticker(ticker, from_date, to_date)
-            output_filename = args.output_dir+os.sep+ticker+os.sep+from_date.isoformat()+"_"+to_date.isoformat()+"_news.json"
-            with open(output_filename, "w+") as f:
-                json.dump(data, f)
-            
-
-
+            if args.debug:
+                print(f"From Date: {from_date.isoformat()}")
+                print(f"To Date: {to_date.isoformat()}")
+            else:
+                data = fetch_news_data_for_ticker(ticker, from_date, to_date)
+                output_filename = args.output_dir+os.sep+ticker+os.sep+from_date.isoformat()+"_"+to_date.isoformat()+"_news.json"
+                with open(output_filename, "w+") as f:
+                    json.dump(data, f)
 
     return
 
