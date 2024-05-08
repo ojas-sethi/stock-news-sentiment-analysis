Add Classification  - Fix normalization bug  - Add Classification results  - Add results to csvs

src/data_analysis_pipeline/bert-sentiment-analysis/classify.py
@@ -3,22 +3,20 @@ import torch
 from urllib import request
 from bs4 import BeautifulSoup
 import csv, argparse, os, sys
-
+import pandas as pd
 sys.path.append("../..")
 
 from utils import NewsArticleDataset
 from transformers import TrainingArguments, Trainer
 import numpy as np
-import operator
+import json, urllib
 from sklearn.metrics import precision_recall_fscore_support, accuracy_score
 
 DEVICE = torch.device('cpu')
 if torch.cuda.is_available():
     DEVICE = torch.device('cuda')
 elif torch.backends.mps.is_available():
-    DEVICE = torch.device('mps') 
-
-
+    DEVICE = torch.device('mps')
 
 # Set global seed
 np.random.seed(42)
@@ -43,42 +41,43 @@ Since we're using the pre-trained model, we need to map labels to the same indic
 code from 
 '''
 
-def test_model(model, test_set, labels, tokenizer):
-    model.eval()
+def test_model(model, test_set : NewsArticleDataset, labels :dict, tokenizer):
+    model.to(torch.bfloat16).eval()
     
     test_data = test_set.get_data()
-    test_labels = torch.tensor([[t_label[l] for l in labels] for t_label in test_set.get_labels()])
+    test_labels = test_set.get_labels() #torch.tensor([[t_label[l] for l in labels] for t_label in test_set.get_labels()]).to(torch.bfloat16)
     y_hat = []
     y = []
 
     for i in range(len(test_labels)):
-        encoded_text = tokenizer(test_data[i], return_tensors="pt", truncation=True, max_length=512, padding=True)
+        encoded_text = tokenizer(test_data[i], return_tensors="pt", padding=True)
         result = model(encoded_text['input_ids'].to(DEVICE))
-        score = torch.nn.functional.softmax(result.logits).detach().cpu().numpy()
-        y_hat.append(labels[int(np.argmax(score, axis=-1))])
-        y.append(labels[int(np.argmax(test_labels[i], axis=-1))])
+        score = torch.nn.functional.softmax(result.logits, dim=-1).detach()
+        y_hat.append(labels[torch.argmax(score, axis=-1).item()])
+        #print(f"Predicted Label: {y_hat[-1]}")
+        #print(f"True Label: {test_labels[i]}")
 
     # Compute accuracy
-    acc = accuracy_score(y, y_hat)
+    acc = accuracy_score(test_labels, y_hat)
 
-    pos_count = 0
+    '''pos_count = 0
     neu_count = 0
     neg_count = 0
     for i in range(len(test_labels)):
-        if int(np.argmax(test_labels[i], axis=-1)) == 0:
+        if np.argmax(test_labels[i], axis=-1).item() == 0:
             pos_count+=1
-        elif int(np.argmax(test_labels[i], axis=-1)) == 1:
+        elif np.argmax(test_labels[i], axis=-1).item() == 1:
             neu_count+=1
-        elif int(np.argmax(test_labels[i], axis=-1)) == 2:
+        elif np.argmax(test_labels[i], axis=-1).item() == 2:
             neg_count+=1
         else:
             print("WTF")
     print(f"pos_count: {pos_count}")
     print(f"neu_count: {neu_count}")
-    print(f"neg_count: {neg_count}")
+    print(f"neg_count: {neg_count}")'''
     
     # Compute Precision
-    precision, recall, f1_score, _ = precision_recall_fscore_support(y, y_hat, labels=labels)
+    precision, recall, f1_score, _ = precision_recall_fscore_support(test_labels, y_hat, labels=list(labels.values()))
     
 
     # Compute Recall
@@ -92,12 +91,12 @@ def test_model(model, test_set, labels, tokenizer):
     print(f"Recall: {recall}")
     print(f"F1 Score: {f1_score}")
 
-    return
+    return acc, precision, recall, f1_score
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset_dir", type=str, default='../../../data/cleaned_data/default', help="Path to the JSON file that contains ticker names and dates.")
-    parser.add_argument("--clean_methods", type=str, default='default', help="Which cleaning technique to apply. Default is to not apply any cleaning technique.")
+    parser.add_argument("--clean_method", type=str, default='default', help="Which cleaning technique to apply. Default is to not apply any cleaning technique.")
     parser.add_argument("--model_path", type=str, default='./finetuned_models', help="Path to the directory where acquired data should be stored.")
     parser.add_argument("--output_dir", type=str, default='../../data/cleaned_data', help="Path to the directory where acquired data should be stored.")
     parser.add_argument("--debug", default=False, help="Setting flag to true disables api requests being sent out.", action="store_true")
@@ -113,15 +112,14 @@ def main():
 
     dataset = NewsArticleDataset()
     dataset.load(args.dataset_dir)
-    model = AutoModelForSequenceClassification.from_pretrained(args.model_path)
-    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+    model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert", num_labels=3)
+    tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert", do_lower_case = False)
 
     labels=[]
-    mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/sentiment/mapping.txt"
+    mapping_link = f"https://huggingface.co/ProsusAI/finbert/raw/main/config.json"
     with request.urlopen(mapping_link) as f:
-        html = f.read().decode('utf-8').split("\n")
-        csvreader = csv.reader(html, delimiter='\t')
-    labels = [row[1] for row in csvreader if len(row) > 1]
+        json_data = json.load(f)
+        labels = {int(k): v for k, v in json_data["id2label"].items()}
 
     model.to(DEVICE)
 
@@ -131,8 +129,15 @@ def main():
     
     #train_model(model, train_set, labels, tokenizer)
 
-    test_model(model, dataset, labels, tokenizer)
+    acc, precision, recall, f1_score = test_model(model, dataset, labels, tokenizer)
     
+    df = pd.DataFrame({'label':labels.values(), 
+                       'accuracy': [acc]*len(precision), 
+                       'precision': precision,
+                       'recall': recall,
+                       'f1_score': f1_score})
+    df.to_csv(f"../sentiment_analysis_results/{args.dataset_dir.split('/')[-1]}.csv")
+
     #model.to(DEVICE).eval()
     #scores = []
 

src/data_analysis_pipeline/bert-sentiment-analysis/fine_tune.py
@@ -9,7 +9,9 @@ sys.path.append("../..")
 from utils import NewsArticleDataset
 from transformers import TrainingArguments, Trainer
 import numpy as np
-import operator
+import urllib
+
+from lsg_converter import LSGConverter
 
 DEVICE = torch.device('cpu')
 if torch.cuda.is_available():
@@ -17,6 +19,12 @@ if torch.cuda.is_available():
 elif torch.backends.mps.is_available():
     DEVICE = torch.device('mps') 
 
+
+mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/sentiment/mapping.txt"
+with urllib.request.urlopen(mapping_link) as f:
+    html = f.read().decode('utf-8').split("\n")
+    csvreader = csv.reader(html, delimiter='\t')
+model_labels = [row[1] for row in csvreader if len(row) > 1]
 
 
 # Set global seed
@@ -42,10 +50,11 @@ Since we're using the pre-trained model, we need to map labels to the same indic
 code from 
 '''
 
-def train_model(model, train_set, labels, tokenizer):
+def train_model(model, train_set, labels, tokenizer, epochs=5):
     model.train()
+
     train_data = train_set.get_data()
-    train_labels = torch.tensor([[t_label[l] for l in labels] for t_label in train_set.get_labels()]).to(DEVICE)
+    train_labels = torch.tensor([[t_label[l] for l in model_labels] for t_label in train_set.get_labels()]).to(torch.bfloat16).to(DEVICE)
 
     optimizier = torch.optim.AdamW(params=model.parameters(), lr=2e-5, weight_decay=0.01)
     optimizier.zero_grad()
@@ -59,6 +68,7 @@ def train_model(model, train_set, labels, tokenizer):
 
         loss.backward()
         optimizier.step()
+
     return
 
 def test_model(model, test_set, labels, tokenizer):
@@ -70,11 +80,12 @@ def test_model(model, test_set, labels, tokenizer):
     y = []
 
     for i in range(len(test_labels)):
-        encoded_text = tokenizer(test_data[i], return_tensors="pt", truncation=True, max_length=512, padding=True)
+        encoded_text = tokenizer(test_data[i], return_tensors="pt", padding=True)
         result = model(encoded_text['input_ids'].to(DEVICE))
-        score = torch.nn.functional.softmax(result.logits).detach().cpu().numpy()
-        y_hat.append(labels[int(np.argmax(score, axis=-1))])
-        y.append(labels[int(np.argmax(test_labels[i], axis=-1))])
+        score = torch.nn.functional.softmax(result.logits, dim=-1).detach()
+        y_hat.append(model_labels[torch.argmax(score, axis=-1).item()])
+        print(f"Predicted Label: {y_hat[-1]}")
+        y.append(labels[torch.argmax(test_labels[i], axis=-1).item()])
 
     acc = [int(y_hat[i]==y[i]) for i in range(len(y_hat))]
 
@@ -99,26 +110,32 @@ def main():
 
     dataset = NewsArticleDataset()
     dataset.load(args.dataset_dir)
-    train_set, test_set = perform_test_train_split(dataset)
-    model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=3)
-    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+    converter = LSGConverter(max_sequence_length=16384)
 
-    labels=[]
+
+    #model, tokenizer = converter.convert_from_pretrained("cardiffnlp/twitter-roberta-base-sentiment", num_labels=3, num_global_tokens=8)
+
+    model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert", num_labels=3)
+    tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert", do_lower_case = False)
+
+
+    '''labels=[]
     mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/sentiment/mapping.txt"
     with request.urlopen(mapping_link) as f:
         html = f.read().decode('utf-8').split("\n")
         csvreader = csv.reader(html, delimiter='\t')
-    labels = [row[1] for row in csvreader if len(row) > 1]
-
-    model.to(DEVICE)
+    labels = [row[1] for row in csvreader if len(row) > 1]'''
+    labels = dataset.labels_list
+    model.to(torch.bfloat16).to(DEVICE)
 
     if args.debug:
         print(f"Labels: {labels}")
         print(f"Using device: {DEVICE}")
-    
-    train_model(model, train_set, labels, tokenizer)
+    for i in range(5):
+        train_set, test_set = perform_test_train_split(dataset)
+        train_model(model, train_set, labels, tokenizer)
 
-    test_model(model, test_set, labels, tokenizer)
+        test_model(model, test_set, labels, tokenizer)
     
     model.save_pretrained(f"{args.output_dir}", from_pt=True)
 